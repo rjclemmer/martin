883ef42814fd1a6edc11d366519544cfdcf5ec35
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-01-01 22:49:47 -0500

    sound after image

--- assets/script.js
+++ assets/script.js
@@ -11,65 +11,74 @@ const fart8 = new Audio("./assets/sounds/fart-08.mp3");
 
 function airTulip () {
     $('#pic').empty();;
-    fart1.play();
+    
     
     var pic1 = document.createElement('img');
     pic1.src = './assets/pics/1.png';
     picEl.appendChild(pic1);
+    fart1.play();
        
 };
 
 function bootyBomb () {
     $('#pic').empty();
-    fart2.play();
+    
     var pic2 = document.createElement('img');
     pic2.src = './assets/pics/2.png';
     picEl.appendChild(pic2);
+    fart2.play();
 };
 
 function buttSneeze () {
     $('#pic').empty();
-    fart3.play();
+    
     var pic3 = document.createElement('img');
     pic3.src = './assets/pics/3.png';
     picEl.appendChild(pic3);
+    fart3.play();
 };
 
 function colonBowlin () {
     $('#pic').empty();
-    fart4.play();
+    
     var pic4 = document.createElement('img');
     pic4.src = './assets/pics/4.png';
     picEl.appendChild(pic4);
+    fart4.play();
 };
 
 function fowlHowl () {
     $('#pic').empty();
-    fart5.play();
+    
     var pic5 = document.createElement('img');
     pic5.src = './assets/pics/5.png';
     picEl.appendChild(pic5);
+    fart5.play();
 }
 function oneManBand () {
     $('#pic').empty();
-    fart6.play();
+    
     var pic6 = document.createElement('img');
     pic6.src = './assets/pics/6.png';
     picEl.appendChild(pic6);
+    fart6.play();
 }
 
 function powerPuff (){
     $('#pic').empty();
-    fart7.play();
+    
     var pic7 = document.createElement('img');
     pic7.src = './assets/pics/7.png';
     picEl.appendChild(pic7);
+    fart7.play();
 }
+
 
 function rumpRoar () {
     $('#pic').empty();
-    fart8.play();
+    
     var pic8 = document.createElement('img');
     pic8.src = './assets/pics/8.png';
     picEl.appendChild(pic8);
+    fart8.play();
 }
