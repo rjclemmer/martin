aa9910e6c26b2b1e25a9f9f81bc67dbf6d218bec
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-01-01 22:16:42 -0500

    Added last sounds and images

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,9 @@ const fart2 = new Audio("./assets/sounds/fart-02.mp3");
 const fart3 = new Audio("./assets/sounds/fart-03.mp3");
 const fart4 = new Audio("./assets/sounds/fart-04.mp3");
 const fart5 = new Audio("./assets/sounds/fart-squeak-02.mp3");
+const fart6 = new Audio("./assets/sounds/fart-05.mp3");
+const fart7 = new Audio("./assets/sounds/fart-07.mp3");
+const fart8 = new Audio("./assets/sounds/fart-08.mp3");
 
 function airTulip () {
     $('#pic').empty();;
@@ -46,4 +49,27 @@ function fowlHowl () {
     var pic5 = document.createElement('img');
     pic5.src = './assets/pics/5.png';
     picEl.appendChild(pic5);
+}
+function oneManBand () {
+    $('#pic').empty();
+    fart6.play();
+    var pic6 = document.createElement('img');
+    pic6.src = './assets/pics/6.png';
+    picEl.appendChild(pic6);
+}
+
+function powerPuff (){
+    $('#pic').empty();
+    fart7.play();
+    var pic7 = document.createElement('img');
+    pic7.src = './assets/pics/7.png';
+    picEl.appendChild(pic7);
+}
+
+function rumpRoar () {
+    $('#pic').empty();
+    fart8.play();
+    var pic8 = document.createElement('img');
+    pic8.src = './assets/pics/8.png';
+    picEl.appendChild(pic8);
 }
